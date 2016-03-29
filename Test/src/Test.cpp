74278113bbefb4f1669b17ac4761608fe6206008
eduardo <eduardos.z199625@gmail.com>
2016-03-28 23:46:04 -0600

    test

---
+++ Test/src/Test.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : Edd
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "test" << endl; // prints test
+	return 0;
+}
